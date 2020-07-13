833775cbe70facda16858660f9845c2f94925c10
Harry Nowakowski <h.nowakowski@uqconnect.edu.au>
2020-07-13 21:30:43 +1000

    Updated stuff

--- calculator_server.py
+++ calculator_server.py
@@ -54,6 +54,20 @@ class CloudCalculator(calculator_pb2_grpc.CloudCalculatorServicer):
         
         # Return the result
         return calculator_pb2.ComputationResponse(
-            responseStatus=calculator_pb2.ComputeStatus.OPERATION_SUCCESSFUL,
-            responseValue=1.0
+            responseStatus=returnStatus,
+            responseValue=returnValue
         )
+
+def _server(port: Text):
+    bind_address = f"[::]:{port}"
+    server = grpc.server(futures.ThreadPoolExecutor())
+    calculator_pb2_grpc.add_CloudCalculatorServicer_to_server(
+        CloudCalculator(), server)
+    server.add_insecure_port(bind_address)
+    server.start()
+    logging.info("Listening on %s.", bind_address)
+    server.wait_for_termination()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    _server(_PORT)
