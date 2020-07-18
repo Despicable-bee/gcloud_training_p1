6246c9a0232aca2a525af74606b1bdfa2cc4e2a5
Despicable-bee <shadow922108@gmail.com>
2020-07-18 22:27:40 +1000

    Forgot to replace '^' with '\' hehe

--- generate_protos.sh
+++ generate_protos.sh
@@ -2,8 +2,8 @@
 echo --------------------------------------------
 echo Calculator Protoc Generator
 echo Generating client and server stubs...
-python -m grpc_tools.protoc --include_imports --include_source_info^
- --proto_path=. --descriptor_set_out=endpoints_api_descriptor.py^
+python -m grpc_tools.protoc --include_imports --include_source_info\
+ --proto_path=. --descriptor_set_out=endpoints_api_descriptor.py\
  --python_out=. --grpc_python_out=. calculator.proto
 echo Done
 echo --------------------------------------------
