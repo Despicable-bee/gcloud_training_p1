714fa076e297873f8896a3298247d130af54608d
Despicable-bee <shadow922108@gmail.com>
2020-07-18 22:26:04 +1000

    Added generate_protos.sh

---
+++ generate_protos.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo --------------------------------------------
+echo Calculator Protoc Generator
+echo Generating client and server stubs...
+python -m grpc_tools.protoc --include_imports --include_source_info^
+ --proto_path=. --descriptor_set_out=endpoints_api_descriptor.py^
+ --python_out=. --grpc_python_out=. calculator.proto
+echo Done
+echo --------------------------------------------
+exit 
